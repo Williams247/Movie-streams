9f6590fc8bf64cb065e452d886efaadcf3cde81f
Williams Alex <williamsalexjr@gmail.com>
2021-11-10 09:23:17 +0100

    Changed the cors for request methods

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer((request, response) => {
   // CORS
   response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  response.setHeader('Access-Control-Allow-Methods', 'GET');
   response.setHeader('Access-Control-Max-Age', 2592000);
 
   // URL object
